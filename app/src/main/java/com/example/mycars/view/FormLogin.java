39d7de0aadb8aff858890ac67ed868cc351e0d04
Gabriel Heldt <gabriel.heldt@develcode.com.br>
2021-09-01 14:45:58 -0300

    Added login authentication and registration

--- app/src/main/java/com/example/mycars/view/FormLogin.java
+++ app/src/main/java/com/example/mycars/view/FormLogin.java
@@ -1,28 +1,40 @@
 package com.example.mycars.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.mycars.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class FormLogin extends AppCompatActivity {
 
     private TextView text_register;
     private TextView text_forgot_password;
     private AppCompatButton button_login;
+    private EditText editEmail;
+    private EditText editPassword;
+    private ProgressBar progressBar;
+    public String[] message = {"Preencha todos os campos"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_login);
-
 
         getSupportActionBar().hide();
         startComponents();
@@ -46,23 +58,61 @@ public class FormLogin extends AppCompatActivity {
         button_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                checkdata();
-                startActivity(new Intent(getApplicationContext(), NavActivity.class));
-                finish();
+                String email = editEmail.getText().toString();
+                String password = editPassword.getText().toString();
+
+                if (email.isEmpty() || password.isEmpty()) {
+                    Snackbar snackbar = Snackbar.make(view, message[0], Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                } else {
+                    authenticateUser(email, password, view);
+                }
             }
         });
     }
 
-    private void checkdata() {
-        EditText editEmail = findViewById(R.id.edit_email);
-        EditText editPassword = findViewById(R.id.edit_password);
-        String email = editEmail.getText().toString();
-        String password = editPassword.getText().toString();
+    private void authenticateUser(String email, String password, View v) {
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    progressBar.setVisibility(View.VISIBLE);
 
-        if (email.equals("g@g.com") && password.equals("123")) {
-            Toast.makeText(this, "Login realizado com sucesso!", Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(this, "Email ou senha incorretos", Toast.LENGTH_SHORT).show();
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            gotoNavActivity();
+                        }
+                    }, 2500);
+                } else {
+                    String error;
+                    try {
+                        throw task.getException();
+                    } catch (Exception e) {
+                        error = "Login Inválido";
+                    }
+                    Snackbar snackbar = Snackbar.make(v, error, Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                }
+            }
+        });
+    }
+
+    private void gotoNavActivity() {
+        startActivity(new Intent(getApplicationContext(), NavActivity.class));
+        finish();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser userLoged = FirebaseAuth.getInstance().getCurrentUser();
+        if (userLoged != null) {
+            gotoNavActivity();
         }
     }
 
@@ -70,5 +120,8 @@ public class FormLogin extends AppCompatActivity {
         text_register = findViewById(R.id.text_register);
         text_forgot_password = findViewById(R.id.text_forgot_password);
         button_login = findViewById(R.id.button_login);
+        editEmail = findViewById(R.id.edit_email);
+        editPassword = findViewById(R.id.edit_password);
+        progressBar = findViewById(R.id.progressbar);
     }
 }
--- app/src/main/java/com/example/mycars/view/FormRegister.java
+++ app/src/main/java/com/example/mycars/view/FormRegister.java
@@ -1,13 +1,32 @@
 package com.example.mycars.view;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.mycars.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class FormRegister extends AppCompatActivity {
 
@@ -15,6 +34,10 @@ public class FormRegister extends AppCompatActivity {
     private EditText editEmail;
     private EditText editPassword;
     private Button buttonRegister;
+    private Button buttonBack;
+    public String[] message = {"Preencha todos os campos", "Cadastro realizado com sucesso!"};
+    public String userId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,10 +46,85 @@ public class FormRegister extends AppCompatActivity {
         getSupportActionBar().hide();
         startComponents();
 
-        buttonRegister.setOnClickListener(new View.OnClickListener() {
+        buttonBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 onBackPressed();
+            }
+        });
+
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = editName.getText().toString();
+                String email = editEmail.getText().toString();
+                String password = editPassword.getText().toString();
+
+                if (name.isEmpty() || email.isEmpty() || password.isEmpty()) {
+                    Snackbar snackbar = Snackbar.make(view, message[0], Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                } else {
+                    registerUser(email, password, view);
+                }
+            }
+        });
+    }
+
+    private void registerUser(String email, String password, View v) {
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+
+                    saveDataUser();
+
+                    Snackbar snackbar = Snackbar.make(v, message[1], Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                } else {
+                    String error;
+                    try {
+                        throw task.getException();
+                    } catch (FirebaseAuthWeakPasswordException e) {
+                        error = "A senha precisa ter no mínimo 6 caracteres";
+                    } catch (FirebaseAuthUserCollisionException e) {
+                        error = "Essa conta já foi cadastrada";
+                    } catch (FirebaseAuthInvalidCredentialsException e) {
+                        error = "Email inválido";
+                    } catch (Exception e) {
+                        error = "Erro ao cadstrar usuário";
+                    }
+                    Snackbar snackbar = Snackbar.make(v, error, Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                }
+            }
+        });
+    }
+
+    private void saveDataUser() {
+        String name = editName.getText().toString();
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        Map<String, Object> user = new HashMap<>();
+        user.put("name", name);
+
+        userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DocumentReference documentReference = db.collection("Users").document(userId);
+        documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                Log.d("db", "Sucesso ao salvar os dados");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("db_error", "Erro ao salvar os dados" + e.toString());
             }
         });
     }
@@ -36,5 +134,6 @@ public class FormRegister extends AppCompatActivity {
         editEmail = findViewById(R.id.edit_email);
         editPassword = findViewById(R.id.edit_password);
         buttonRegister = findViewById(R.id.button_register);
+        buttonBack = findViewById(R.id.button_back);
     }
 }
